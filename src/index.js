b98b5c9e6bbeee112ec0f9ea1a089b08f6c99f0a
synacten <blackhorns9988@gmail.com>
2019-01-18 23:22:19 +0200

    remove static bsgrid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "@babel/polyfill";
 import style from "./scss/main.scss";
 import scripts from './js/client'
+import grid from 'bootstrap-4-grid/scss/grid.scss'
+// import swiper from 'swiper/dist/css/swiper.css'
--- src/js/client.js
+++ src/js/client.js
@@ -5,7 +5,12 @@ import Swiper from 'swiper/dist/js/swiper'
 const mySwiper = new Swiper('.swiper-container', {
     speed: 400,
     spaceBetween: 100,
-    loop: true
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
 });
 
 
